81a5ffdb8b342c51193d99f176fa17f413cf62e4
Chad Scherrer <chad.scherrer@gmail.com>
2021-03-03 12:20:48 -0800

    fixing up chainvec

--- src/utils.jl
+++ src/utils.jl
@@ -1,6 +1,7 @@
 using MCMCDiagnostics
 export expect
 using Measurements
+using StructArrays: StructVector
 
 # Expected value of x log-weighted by ℓ
 function expect(ℓ,x)
@@ -38,19 +39,42 @@ macro cleanbreak(ex)
 end
 
 """
-    chainvec(x)
+    chainvec(x::T, n=1)
 
-Return a vector used to store [x] in a SampleChain.
+Return a vector used to store the type T in a SampleChain. Satisfies the law
+    chainvec(x,n)[1] == x
 
-Note that `x` should correspond to information stored in a single sample.
+If `n>1`, the remaining entries will be `undef`. Note that `x` should correspond
+to information stored in a single sample. 
 """
 function chainvec end
 
 
-chainvec(nt::NamedTuple) = TupleVector([nt])
-chainvec(x::Real) = ElasticVector([x])
+function chainvec(nt::NamedTuple, n=1)
+    tv = TupleVector(undef, nt, n)
+    @inbounds tv[1] = nt
+    return tv
+end
 
-function chainvec(x::T) where T
-    isstructtype(T) && return ElasticVector(StructArray([x]))
-    return ElasticVector([x])
+function chainvec(x::T, n=1) where {T<:Real}
+    ev = ElasticVector{T}(undef, n)
+    @inbounds ev[1] = x
+    return ev
+end
+
+function chainvec(x::T, n=1) where {T}
+    if isstructtype(T)
+        v = replace_storage(ElasticVector, StructVector{T}(undef, n))
+        @inbounds v[1] = x
+        return v
+    end
+    ev = ElasticVector{T}(undef, n)
+    @inbounds ev[1] = x
+    return ev
+end
+
+function chainvec(x::DenseArray{T,N}, n=1) where {T,N}
+    nv = nestedview(ElasticArray{T,N+1}(undef, size(x)..., n), N)
+    @inbounds nv[1] = x
+    return nv
 end
